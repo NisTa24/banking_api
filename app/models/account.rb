22e0ad38dd3809cd294d84f0ed2893fb386fc109
Nishan Tamang <n.tamang@paicon.com>
2025-07-19 14:21:05 +0200

    update account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,23 @@
 class Account < ApplicationRecord
+  has_many :outgoing_transactions,
+           class_name: "Transaction",
+           foreign_key_id: "from_account_id",
+           dependent: :restrict_with_exception
+
+  has_many :incoming_transactions,
+           class_name: "Transaction",
+           foreign_key_id: "to_account_id",
+           dependent: :restrict_with_exception
+
+  def balance
+    BigDecimal(super.to_s)
+  end
+
+  def balance=(value)
+    super(BigDecimal(value.to_s))
+  end
+
+  def sufficient_funds?(amount)
+    balance >= BigDecimal(amount.to_s)
+  end
 end
